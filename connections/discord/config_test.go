7aeadfc179dfc5fd188a059a4d34adac0c0c7a67
miodzie <miodzie@protonmail.com>
2022-11-29 20:46:29 -0500

    Fix config_test.go

--- connections/discord/config_test.go
+++ connections/discord/config_test.go
@@ -4,29 +4,26 @@
 
 package discord
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestParseConfig_parses_into_Config_struct(t *testing.T) {
 	val := make(map[string]interface{})
 	val["type"] = "discord"
 	val["token"] = "my_token"
 	val["admins"] = []interface{}{"alice"}
-	val["mods"] = []interface{}{"myplugin"}
+	mods := make(map[string]interface{})
+	mods["my_plugin"] = struct{}{}
+	val["mods"] = mods
 
 	cfg, err := ParseConfig(val)
-	if err != nil {
-		t.Errorf("expected error: %s\n", err)
-	}
 
-	if cfg.Token != val["token"] {
-		t.Error("token was not parsed")
-	}
-	if len(cfg.Admins) != 1 {
-		t.Error("admins was not parsed")
-	}
-	if len(cfg.Mods) != 1 {
-		t.Error("mods was not parsed")
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, val["token"], cfg.Token)
+	assert.Len(t, cfg.Admins, 1)
+	assert.Len(t, cfg.Mods, 1)
 }
 
 func TestParseConfig_returns_error_if_type_is_not_discord(t *testing.T) {
@@ -34,7 +31,6 @@ func TestParseConfig_returns_error_if_type_is_not_discord(t *testing.T) {
 	val["type"] = "irc"
 
 	_, err := ParseConfig(val)
-	if err != ErrIncorrectType {
-		t.Errorf("expected error: %s\n", err)
-	}
+
+	assert.Equal(t, ErrIncorrectType, err)
 }

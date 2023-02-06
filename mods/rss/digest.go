cf2ab9da7109a12d439a06008d433c15b4d3121a
miodzie <miodzie@protonmail.com>
2023-02-05 22:59:17 -0500

    Cleaning, add start of digest.go

---
+++ mods/rss/digest.go
@@ -0,0 +1,21 @@
+package rss
+
+// Digest is a Deliverable that pools given deliveries until a configured threshold,
+// then delivers them all at once.
+// Plans are for a digest by Email, and/or by viewable by a private link.
+
+// Digest should probably accept chan Notification, not chan Deliverable.
+// This allows more nuance into formatting, not sure yet.
+
+type Digest struct {
+}
+
+func (d Digest) Address() string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d Digest) Deliver(deliver func(address string, content string) error) {
+	//TODO implement me
+	panic("implement me")
+}

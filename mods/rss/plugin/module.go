c999092f475bbb924bbb6683b973e6a76c5fe069
miodzie <miodzie@protonmail.com>
2023-02-11 12:52:57 -0500

    no time to wait

--- mods/rss/plugin/module.go
+++ mods/rss/plugin/module.go
@@ -42,7 +42,6 @@ func (mod *RssMod) Start(stream walter.Stream, actions walter.Actions) error {
 }
 
 func (mod *RssMod) checkFeeds() {
-	time.Sleep(time.Minute * 1)
 	processor := rss.NewProcessor(mod.Fetcher, mod.Repository)
 	for mod.running {
 		log.Info("Processing feed subscriptions...")
